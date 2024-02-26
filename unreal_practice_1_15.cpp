97368cd80ed81ba78ebc5f39e62a58ba89554479
Vadim Lemmer <lemmeronne@gmail.com>
2024-02-26 23:21:43 +0600

    feat: нахождение чётных и нечётных чисел

---
+++ unreal_practice_1_15.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+void printNumber(int n)
+{
+    std::cout << n << " ";
+}
+
+void findOddNumbers(int limit, bool isOdd)
+{
+    for (int i = int(isOdd); i <= limit; i += 2)
+    {
+        printNumber(i);
+    }
+}
+
+int main()
+{
+    const int n = 10;
+    for (int i = 0; i <= n; i += 2)
+    {
+        printNumber(i);
+    }
+
+    std::cout << "\n";
+    findOddNumbers(10, true);
+    std::cout << "\n";
+    findOddNumbers(10, false);
+}
